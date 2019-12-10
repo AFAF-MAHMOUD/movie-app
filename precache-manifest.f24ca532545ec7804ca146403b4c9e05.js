89e6752f03fc5ebc51c24086b5d008f462d95ae9
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-10 10:47:55 +0100

    Updates

---
+++ precache-manifest.f24ca532545ec7804ca146403b4c9e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a672857ebfa76c0dfd30410936c8f4a9",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "62b21e679b2d8fcea650",
+    "url": "/movie-app/static/css/main.fcc9e482.chunk.css"
+  },
+  {
+    "revision": "1289da728340874c2a15",
+    "url": "/movie-app/static/js/2.10a3c90c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-app/static/js/2.10a3c90c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62b21e679b2d8fcea650",
+    "url": "/movie-app/static/js/main.71a81c87.chunk.js"
+  },
+  {
+    "revision": "2d5fdcfbb36f03b152d8",
+    "url": "/movie-app/static/js/runtime-main.ee15cbd1.js"
+  }
+]);
